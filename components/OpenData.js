7720b7b90c0de52139bb45e01ed802450e44970d
febriano <rinoakbr@gmail.com>
2021-02-20 11:02:04 +0700

    kurang map forest loss

---
+++ components/OpenData.js
@@ -0,0 +1,33 @@
+import react from 'react'
+export default function OpenData(){
+    return (<>
+  <section class="px-2 py-32 bg-white md:px-0">
+  <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+    <div class="flex flex-wrap items-center sm:-mx-3">
+      <div class="w-full md:w-1/2 md:px-3">
+        <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+          <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+            <span class="block xl:inline">Open Data </span>
+            <span class="block text-indigo-600 xl:inline">Used In This Website</span>
+          </h1>
+          <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Click on learn more to get the data used on this project and how to create it.</p>
+          <div class="relative flex flex-col sm:flex-row sm:space-x-4">
+            <a href="#_" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+             Learn More
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+            </a>
+          
+          </div>
+        </div>
+      </div>
+      <div class="w-full md:w-1/2">
+        <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+            <img src="https://cdn.devdojo.com/images/november2020/hero-image.jpeg"/>
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    </>);
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Map from "../components/Map";
 import Air from "../components/Air";
+import Plant from "../components/Plant";
+import Donate from "../components/Donate";
+import Contact from "../components/Contact";
+import OpenData from "../components/OpenData";
 export default function Home() {
   return (
     <div>
@@ -12,6 +16,11 @@ export default function Home() {
       <Navbar />
       <Map />
       <Air />
+      <Plant />
+      <Donate />
+      <OpenData />
+      <Contact />
+     
     </div>
   );
 }
