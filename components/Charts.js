c83644766cd57f467904ac14c7c05f1b3e89d1ef
febriano <rinoakbr@gmail.com>
2021-02-21 16:28:40 +0700

    static props rusak

--- components/Charts.js
+++ components/Charts.js
@@ -66,11 +66,11 @@ export default function Map() {
       setEndYear([]);
       return;
     }
-   
+
     const countrySelect = country.filter(
       (a) => a.country === e.target.value
     )[0];
-    
+
     setStartYear(countrySelect.year);
     setEndYear(countrySelect.year);
     setPickCountry(e.target.value);
@@ -114,95 +114,128 @@ export default function Map() {
       }))
       .filter(
         (a) =>
-          a.country === pickCountry && Number(a.year) >= Number(pickStartYear) && Number(a.year) <= Number(pickEndYear)
+          a.country === pickCountry &&
+          Number(a.year) >= Number(pickStartYear) &&
+          Number(a.year) <= Number(pickEndYear)
       );
-    setYearBefore(rows
-      .map((a) => ({
-        country: a["Entity,"],
-        year: Number(a["Year,"]),
-        area: a["Forest area"],
-      }))
-      .filter(
-        (a) =>
-          a.country === pickCountry && Number(a.year) === Number(pickStartYear)-1
-      ))
-    setChartData({year:_.pluck(result,"year"),data:result})
-    return ;
+    setYearBefore(
+      rows
+        .map((a) => ({
+          country: a["Entity,"],
+          year: Number(a["Year,"]),
+          area: a["Forest area"],
+        }))
+        .filter(
+          (a) =>
+            a.country === pickCountry &&
+            Number(a.year) === Number(pickStartYear) - 1
+        )
+    );
+    setChartData({ year: _.pluck(result, "year"), data: result });
+    return;
   }
   return (
     <>
-      <div id="forest-chart" className="container mx-auto flex py-3 items-center justify-center flex-col">
+      <div
+        id="forest-chart"
+        className="container mx-auto flex py-3 items-center justify-center flex-col"
+      >
         <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-gray-900">
           Forest Data Per Year
         </h1>
       </div>
       <div
-        class="container mx-auto flex sm:flex-nowrap flex-wrap items-center justify-center"
+        className="container mx-auto flex sm:flex-nowrap flex-wrap items-center justify-center"
         style={{ display: error.state ? "block" : "none" }}
       >
-        <div class="alert alert-error">
-          <div class="flex-1">
-            <label class="mx-3">{error.message}</label>
+        <div className="alert alert-error">
+          <div className="flex-1">
+            <label className="mx-3">{error.message}</label>
           </div>
-          <div class="flex-none">
-            <button class="btn btn-sm btn-ghost mr-2" onClick={closeMessage}>
+          <div className="flex-none">
+            <button
+              className="btn btn-sm btn-ghost mr-2"
+              onClick={closeMessage}
+            >
               Close
             </button>
           </div>
         </div>
       </div>
 
-      <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-5 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <ForestChart year={chartData.year} data={chartData.data} yearBefore={yearBefore} />
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-5 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <ForestChart
+              year={chartData.year}
+              data={chartData.data}
+              yearBefore={yearBefore}
+            />
           </div>
-          <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
               Search your Country Or Continent Forest Data
             </h2>
-            <p class="leading-relaxed mb-5 text-gray-600">
+            <p className="leading-relaxed mb-5 text-gray-600">
               Fill in the data below
             </p>
-            <div class="relative mb-4">
-              <label for="name" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-600">
                 Country or Continent
               </label>
               <select
                 onChange={handleChange}
-                class="select select w-full max-w-xs"
+                className="select select w-full max-w-xs"
               >
                 <option value={""}>Select your country or continent</option>
                 {country.map((a) => (
-                  <option value={a.country}>{a.country}</option>
+                  <option key={a.country} value={a.country}>
+                    {a.country}
+                  </option>
                 ))}
               </select>
             </div>
-            <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
                 Start Year
               </label>
-              <select onChange={(e)=>setPickStartYear(e.target.value)} class="select select w-full max-w-xs">
+              <select
+                onChange={(e) => setPickStartYear(e.target.value)}
+                className="select select w-full max-w-xs"
+              >
                 <option value={""}>Select Start Year</option>
                 {startYear.map((a) => (
-                  <option value={a}>{a}</option>
+                  <option key={a} value={a}>
+                    {a}
+                  </option>
                 ))}
               </select>
             </div>
-            <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
                 End Year
               </label>
-              <select onChange={(e)=>setPickEndYear(e.target.value)} class="select select w-full max-w-xs">
+              <select
+                onChange={(e) => setPickEndYear(e.target.value)}
+                className="select select w-full max-w-xs"
+              >
                 <option value={""}>Select End Year</option>
                 {endYear.map((a) => (
-                  <option value={a}>{a}</option>
+                  <option key={a} value={a}>
+                    {a}
+                  </option>
                 ))}
               </select>
             </div>
             <button
               onClick={checkData}
-              class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
             >
               Search
             </button>
--- components/Donate.js
+++ components/Donate.js
@@ -1,39 +1,39 @@
 import react from 'react'
 export default function Donate(){
     return (<>
-   <section id="donate" class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
-    <div class="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
+   <section id="donate" className="relative py-16 bg-white min-w-screen animation-fade animation-delay">
+    <div className="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
        
-        <h3 class="mt-1 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
+        <h3 className="mt-1 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
             Donate To This Non Profit Organization Dedicated For Planting Plants</h3>
 
         <div
-            class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How does it work?</h3>
-            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">Our platform works with your content to
+            className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How does it work?</h3>
+            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">Our platform works with your content to
                 provides
                 insights and metrics on how you can grow your business and scale your infastructure.</p>
         </div>
         <div
-            class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">Do you offer team pricing?</h3>
-            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">Yes, we do! Team pricing is available for
+            className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">Do you offer team pricing?</h3>
+            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">Yes, we do! Team pricing is available for
                 any
                 plan. You can take advantage of 30% off for signing up for team pricing of 10 users or more.</p>
         </div>
         <div
-            class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How do I make changes and configure my
+            className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How do I make changes and configure my
                 site?
             </h3>
-            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">You can easily change your site settings
+            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">You can easily change your site settings
                 inside
                 of your site dashboard by clicking the top right menu and clicking the settings button.</p>
         </div>
         <div
-            class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How do I add a custom domain?</h3>
-            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">You can easily change your site settings
+            className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How do I add a custom domain?</h3>
+            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">You can easily change your site settings
                 inside
                 of your site dashboard by clicking the top right menu and clicking the settings button.</p>
         </div>
